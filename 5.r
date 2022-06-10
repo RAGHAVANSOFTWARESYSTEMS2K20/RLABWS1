14daae39fc7bf5acbc45762431be10e29cecb391
Raghavan M <76263002+RAGHAVANSOFTWARESYSTEMS2K20@users.noreply.github.com>
2022-06-10 09:17:57 +0530

    Update 5.r

--- 5.r
+++ 5.r
@@ -1,4 +1,13 @@
 # Import lpSolve package
+#A company has two grades of inspector 1 and 2, who are to be assigned for a
+#uality control inspection. It is required that at least 2,000 pieces be inspected per
+#8 – hour day. Grade 1 inspector can check pieces at the rate of 40 with an
+#accuracy of 97 %. Grade 2 inspector checks at the rate of 30 pieces per hour with
+#an accuracy of 95 %. The wage rate of a Grade 1 inspector is Rs.5 per hour while
+#that of a Grade 2 inspector is Rs.4 per hour. An error made by an inspector costs
+#Rs.3 to the company. There are only nine Grade 1 inspectors and eleven Grade 2
+#inspectors available in the company. The company wishes to assign work to the
+#available inspectors so as to minimize the daily inspection cost.
 library(lpSolve)
 
 # Set coefficients of the objective function
